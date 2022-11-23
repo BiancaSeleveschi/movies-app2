e13145ae4c385fdbd88cd312ac62bd8ba58172ac
BiancaSeleveschi <bianca.seleveschi@gmail.com>
2022-11-23 20:31:27 +0200

    add first project files - add bootstrap library - create array of movies - create methods: addNewMovie, deleteMovie - create functionalities in template for seen movies - add modal for input and button search

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,106 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>FILME</h1>
+    <div class="input-group mb-3" >
+      <input v-model="movieSearched.title" type="text" class="form-control w-auto" placeholder="Numele filmului">
+      <button class="btn btn-secondary">Search</button>
+      <div>{{ movieSearched }}</div>
+    </div>
+    <div class=" p-2 m-auto">
+      <input v-model="newMovie.title" type="text" placeholder="Numele filmului">
+      <input v-model="newMovie.gendre" type="text" placeholder="Gen">
+      <input v-model="newMovie.producer" type="text" placeholder="Regizor">
+      <input v-model="newMovie.year" type="text" placeholder="Anul aparitiei">
+      <input v-model="newMovie.seen" type="text" placeholder="Vazut">
+      <button @click="addNewMovie">Adauga film</button>
+    </div>
+
+    <h2 class="p-3" @click="showMovie = !showMovie">Lista de filme</h2>
+    <div v-show="showMovie">
+      <div v-for="(movie, index) in movies" v-bind:key="index" id="movies-div"
+           class="col border border-3 border border-dark rounded-4 m-auto p-3 text-dark fst-italic my-2">
+        <p @click="showIndexDetails = showIndexDetails === -1 ? index : -1"> {{ movie.title }}</p>
+        <div v-show="showIndexDetails === index">
+          <p>Genul: {{ movie.gendre }}</p>
+          <p>Producator: {{ movie.producer }}</p>
+          <p>Anul aparitiei: {{ movie.year }}</p>
+          <p>Vazut: {{ movie.seen }}</p>
+          <button @click="seenButton = !seenButton" class="btn btn-warning mx-2">{{
+              seenButton ? "✔️" : "Seen"
+            }}
+          </button>
+          <button @click="deleteMovie" class="btn btn-success">Delete</button>
+          <div v-show="seenButton">
+            <input v-model="seenInput" class="my-2" type="number" placeholder="year">
+            <p>{{ seenInput }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      movies: [
+        {
+          title: "Se7en",
+          gendre: "Drama",
+          producer: "James Mc. Teigue",
+          year: 2000,
+          seen: false
+        },
+        {
+          title: "Blood Diamond",
+          gendre: "Drama",
+          producer: "Edward Zwik",
+          year: 2007,
+          seen: false
+        },
+        {
+          title: "Changeling",
+          gendre: "Mister",
+          producer: "Clint Eastwood",
+          year: 2011,
+          seen: false
+        },
+        {
+          title: "Alive",
+          gendre: "Aventura",
+          producer: "Franc Marshall",
+          minutesLength: 127,
+          year: 2005,
+          seen: true
+        }
+      ],
+      newMovie: {
+        title: "",
+        gendre: "",
+        producer: "",
+        year: '',
+        seen: '',
+      },
+      movieSearched: "",
+      showMovie: false,
+      showIndexDetails: -1,
+      seenButton: false,
+      seenInput: '',
+      showSeenMovie: false,
+      showIndexInput: -1,
+    }
+  },
+  methods: {
+    addNewMovie() {
+      this.movies.push(this.newMovie)
+    },
+    deleteMovie(index) {
+      this.movies.splice(index, 1)
+      this.showIndexDetails = this.showIndexDetails === -1 ? index : -1;
+    },
   }
 }
 </script>
@@ -24,5 +113,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: cornflowerblue;
+}
+
+html {
+  background: lavenderblush;
+}
+
+#movies-div {
+  background: blue;
+  width: 350px;
+}
+
+p {
+  color: cornsilk;
 }
 </style>
